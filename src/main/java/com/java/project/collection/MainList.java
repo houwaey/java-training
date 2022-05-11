8e6363adba733efd7cf26bde60c54ed74d958c1b
Houwaey- <nhoj115@gmail.com>
2022-05-11 19:49:17 +0800

    Added List collection interface

---
+++ src/main/java/com/java/project/collection/MainList.java
@@ -0,0 +1,24 @@
+package com.java.project.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+//        list.add(0, "d");
+//        System.out.println("Index=1: " + list.get(1));
+//        list.set(2, "c++");
+//        list.remove(3);
+//        System.out.println(list);
+//        System.out.println(list.indexOf("c++"));
+//        System.out.println(list.indexOf("d++")); // -1
+
+//        list.add("a");
+//        System.out.println(list.lastIndexOf("a")); // index of the last occurrence of this object
+//        System.out.println(list.subList(0, 2)); // from=inclusive, to=exclusive
+    }
+}
